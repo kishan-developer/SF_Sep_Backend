cc521a453aa4ba21c89aa8af8c0007cf68bb062b
kishan-developer <139308962+kishan-developer@users.noreply.github.com>
2025-08-02 00:18:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use("/api/v1", router);
 
 // Default route
 app.get("/", (req, res) => {
-  res.send("Welcome to the API root SRIJNFBS");
+  res.send("Welcome to the API root SRIJNFBS...");
 });
 
 // 404 handler
