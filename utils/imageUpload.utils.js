3060d145c894c0c7cc17494404d46510d8c1b5bf
kishan-developer <139308962+kishan-developer@users.noreply.github.com>
2025-08-02 11:46:38 +0530

    Update imageUpload.utils.js

--- utils/imageUpload.utils.js
+++ utils/imageUpload.utils.js
@@ -47,22 +47,63 @@
 
 
 // ------------------------------------------------
+// const fs = require("fs");
+// const path = require("path");
+
+// const imageUploader = async (file, imageFileName, productName) => {
+//     const folder = `uploads/images/products/${productName}`;
+//     const uploadPath = path.join(__dirname, "..", folder);
+
+//     if (!fs.existsSync(uploadPath)) {
+//         fs.mkdirSync(uploadPath, { recursive: true });
+//     }
+
+//     const moveFile = (fileObj) => {
+//         return new Promise((resolve, reject) => {
+//             const ext = path.extname(fileObj.name);
+//             const fileName = `${imageFileName}-${Date.now()}-${Math.floor(Math.random() * 1e9)}${ext}`;
+//             const filePath = path.join(uploadPath, fileName);
+
+//             fileObj.mv(filePath, (err) => {
+//                 if (err) return reject(err);
+//                 resolve(`${process.env.BACKEND_URL}/images/products/${productName}/${fileName}`);
+//             });
+//         });
+//     };
+
+//     if (Array.isArray(file)) {
+//         const uploaded = [];
+//         for (const f of file) {
+//             uploaded.push(await moveFile(f));
+//         }
+//         return uploaded;
+//     }
+
+//     return await moveFile(file);
+// };
+
+// module.exports = imageUploader;
+
+
+
+
+/// store image in VPS ROOT/Uploads
+
 const fs = require("fs");
 const path = require("path");
 
 const imageUploader = async (file, imageFileName, productName) => {
-    const folder = `uploads/images/products/${productName}`;
-    const uploadPath = path.join(__dirname, "..", folder);
-
-    if (!fs.existsSync(uploadPath)) {
-        fs.mkdirSync(uploadPath, { recursive: true });
+    const baseUploadPath = `/root/uploads/image/products/${productName}`;
+    
+    if (!fs.existsSync(baseUploadPath)) {
+        fs.mkdirSync(baseUploadPath, { recursive: true });
     }
 
     const moveFile = (fileObj) => {
         return new Promise((resolve, reject) => {
             const ext = path.extname(fileObj.name);
             const fileName = `${imageFileName}-${Date.now()}-${Math.floor(Math.random() * 1e9)}${ext}`;
-            const filePath = path.join(uploadPath, fileName);
+            const filePath = path.join(baseUploadPath, fileName);
 
             fileObj.mv(filePath, (err) => {
                 if (err) return reject(err);
@@ -83,3 +124,4 @@ const imageUploader = async (file, imageFileName, productName) => {
 };
 
 module.exports = imageUploader;
+
