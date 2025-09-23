65079c829f915f9156e728a1d55f7ecc745fa300
kishan-developer <139308962+kishan-developer@users.noreply.github.com>
2025-09-23 12:33:52 +0530

    Update blog.routes.js

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,14 +1,26 @@
-import express from 'express'
-import { createBlog, deleteBlog, getBlogById, getBlogs, getBlogsAdmin, updateBlog } from '../controller/blog.controller.js';
-import { isAdmin, isAuthenticated } from '../middleware/auth.middleware.js';
+const express = require('express');
+const {
+  createBlog,
+  deleteBlog,
+  getBlogById,
+  getBlogs,
+  getBlogsAdmin,
+  updateBlog
+} = require('../controller/blog.controller.js');
+const {
+  isAdmin,
+  isAuthenticated
+} = require('../middleware/auth.middleware.js');
 
-export const blogRouter = express.Router();
+const blogRouter = express.Router();
 
 blogRouter.post('/', isAuthenticated, isAdmin, createBlog);
 blogRouter.put('/:id', isAuthenticated, isAdmin, updateBlog);
 blogRouter.delete('/:id', isAuthenticated, isAdmin, deleteBlog);
 blogRouter.get('/', isAuthenticated, isAdmin, getBlogsAdmin);
 
-// User Get Blogs 
+// User Get Blogs
 blogRouter.get('/all', getBlogs);
 blogRouter.get('/:id', getBlogById);
+
+module.exports = blogRouter;
